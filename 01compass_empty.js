712bce5084553c74716d9a648e4887801c839f6a
Johannes Birlinger <johannesbirlinger@byod-109-225.pub.uniarts.fi>
2018-02-22 12:15:25 +0200

    Added compass points

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -25,7 +25,7 @@ oriented = function(e) {
                 text_area.textContent = "west 5";
         }
 
-         text_area.textContent = compassHeading;
+         //text_area.textContent = compassHeading;
         //calls function to alter content based on heading
        // myOrientation();
     }
