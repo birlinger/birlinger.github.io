567912fb17ce2db99de0b08f9889e94541fd7b19
Johannes Birlinger <johannesbirlinger@Johannes-Birlingers-MacBook-Pro.local>
2018-02-23 21:13:45 +0200

    for Venla

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -10,56 +10,56 @@ oriented = function(e) {
         
 
         if(compassHeading > 315 || compassHeading < 10) {
-                text_area.textContent = "Welcome!";
+                text_area.textContent = "I";
         }
 
         if(compassHeading >= 10 && compassHeading < 30) {
-                text_area.textContent = "Instructions:";
+                text_area.textContent = "love";
         }
 
         if(compassHeading >= 30 && compassHeading < 45) {
-                text_area.textContent = "1:";
+                text_area.textContent = "you";
         }
 
         if(compassHeading >= 45 && compassHeading < 75) {
-                text_area.textContent = "go to google streetview.";
+                text_area.textContent = "very";
         }
 
         if(compassHeading >= 75 && compassHeading < 95) {
-                text_area.textContent = "2:";
+                text_area.textContent = "much,";
         }
 
         if(compassHeading >= 95 && compassHeading < 105) {
-                text_area.textContent = "find your location.";
+                text_area.textContent = "Venla!";
         }
 
         if(compassHeading >= 105 && compassHeading < 125) {
-                text_area.textContent = "3:";
+                text_area.textContent = "It";
         }
 
         if(compassHeading >= 125 && compassHeading < 145) {
-                text_area.textContent = "explore ";
+                text_area.textContent = "feels";
         }
 
         if(compassHeading >= 145 && compassHeading < 165) {
-                text_area.textContent = "any ";
+                text_area.textContent = "safe";
         }
 
         if(compassHeading >= 165 && compassHeading < 190) {
-                text_area.textContent = "of";
+                text_area.textContent = "and";
         }
 
         if(compassHeading >= 190 && compassHeading < 225) {
-                text_area.textContent = "the";
+                text_area.textContent = "inspiring";
         }
 
 
         if(compassHeading >= 225 && compassHeading < 265) {
-                text_area.textContent = "four";
+                text_area.textContent = "and very exciting";
         }
 
         if(compassHeading >= 265 && compassHeading < 315) {
-                text_area.textContent = "realities";
+                text_area.textContent = "to be with you!!!";
         }
 
          //text_area.textContent = compassHeading;
